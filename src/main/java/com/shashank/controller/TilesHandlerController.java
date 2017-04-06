764212253955954791c3e6ae4805ad4a7fb6ddad
Shashank-Java <shashankmani02@gmail.com>
2017-04-06 11:57:00 +0530

    added TilesHandlerController

--- src/main/java/com/shashank/controller/TilesHandlerController.java
+++ src/main/java/com/shashank/controller/TilesHandlerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TilesHandlerController {
 
+
 	@RequestMapping("/home.htm")
 	public String homePage() {
 		System.out.println("home");
